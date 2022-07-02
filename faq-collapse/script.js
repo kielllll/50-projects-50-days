591ad16867f93e9713b767e8124ba5cf76a3868c
kielllll <eatarranza@gmail.com>
2022-07-03 02:17:22 +0800

    update: folder structure

--- faq-collapse/script.js
+++
@@ -1,5 +0,0 @@
-const toggleButtons = document.querySelectorAll('.faq-toggle')
-
-toggleButtons.forEach((toggleButton) =>
-  toggleButton.addEventListener('click', () => toggleButton.parentNode.classList.toggle('active'))
-)
--- hidden-search-widget/script.js
+++
@@ -1,8 +0,0 @@
-const search = document.querySelector('.search')
-const button = document.querySelector('.button')
-const input = document.querySelector('.input')
-
-button.addEventListener('click', () => {
-  search.classList.toggle('active')
-  input.focus()
-})
